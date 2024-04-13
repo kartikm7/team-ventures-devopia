94d4f1e0031d818dcf41c085c0996cd372538391
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-04-13 17:09:25 +0530

    feat: landing

--- client/src/app/form/teacher-form/page.tsx
+++ client/src/app/form/teacher-form/page.tsx
@@ -1,9 +1,169 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 
-export default function form(){
-  return <div className="w-full h-screen flex justify-center items-center">
-      <div className="">
+type FormFieldName =
+  | "school"
+  | "specialization"
+  | "phoneNumber"
+  | `specialization.${number}`;
 
+const formSchema = z.object({
+  school: z.string(),
+  specialization: z.array(z.string()),
+  phoneNumber: z.string(),
+});
+
+export default function Login() {
+  const router = useRouter();
+  const [specializations, setSpecializations] = useState<string[]>([""]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      school: "",
+      specialization: [],
+      phoneNumber: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+
+    try {
+      // Perform login using other credentials (e.g., social login).
+      // For this example, it's assumed that the login is handled elsewhere.
+      router.push("/dashboard");
+      toast(`Welcome!`);
+    } catch (error) {
+      toast(`Error: ${error}`);
+    }
+  }
+
+  const addSpecializationField = () => {
+    if (specializations.length < 6) {
+      setSpecializations([...specializations, ""]);
+    }
+  };
+
+  const handleSpecializationChange = (index: number, value: string) => {
+    const updatedSpecializations = [...specializations];
+    updatedSpecializations[index] = value;
+    setSpecializations(updatedSpecializations);
+  };
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="grid grid-cols-2 gap-20">
+        <div className="">
+          <Image alt="teacher-image" src={'/images/teacher.jpg'} width={500} height={500} />
+        </div>
+        <div className="">
+          <Card>
+            <CardHeader>
+              <CardTitle>Fill in the details</CardTitle>
+              <CardDescription>Approx 2 minutes.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="school"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>School</FormLabel>
+                        <FormControl>
+                          <Input placeholder="School" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="h-3/4">
+                    {specializations.map((value, index) => (
+                      <FormField
+                        key={index}
+                        control={form.control}
+                        name={`specialization_${index}` as FormFieldName}
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Specialization {index + 1}</FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="Enter specialization"
+                                value={value}
+                                onChange={(e) =>
+                                  handleSpecializationChange(
+                                    index,
+                                    e.target.value
+                                  )
+                                }
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    ))}
+                  </div>
+
+                  <Button
+                    type="button"
+                    size={"sm"}
+                    variant={"secondary"}
+                    onClick={addSpecializationField}
+                    disabled={specializations.length === 6}
+                  >
+                    Add Specialization
+                  </Button>
+                  <FormField
+                    control={form.control}
+                    name="phoneNumber"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Phone Number</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Phone Number" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Submit</Button>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-  </div>
+    </div>
+  );
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,14 +2,179 @@
 import Image from "next/image";
 import { SignUp } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import Link from 'next/link'
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
+  const name = "Learn Space";
   return (
-    <main className="flex justify-center items-center mt-10">
-      <Link href="/login">
-        <Button className="text-blue-background font-text">Login</Button>
-      </Link>
+    <main className="w-full h-screen ">
+      <div className="bg-gradient-to-b from-green-dark to-white w-full h-screen flex justify-between items-center ">
+        <div className="flex flex-col md:block text-left mx-auto text-5xl">
+          <p className="text-white">
+            <span className="font-semibold">Studying online</span> is now,{" "}
+            <br /> much better!
+          </p>
+          <Button className="mt-5" variant={"secondary"}>
+            Get Started!
+          </Button>
+        </div>
+        <div className="self-end mx-auto hidden md:block">
+          <Image
+            src={"/images/landing.png"}
+            alt="hero image"
+            width={600}
+            height={600}
+          />
+        </div>
+      </div>
+      <div className="w-full h-screen flex flex-col gap-20 justify-center items-center ">
+        <div className="">
+          <h1 className="text-3xl font-semibold">
+            <span className="text-green-heading">All-In-One</span>{" "}
+            <span className="">Education Platform</span>
+          </h1>
+        </div>
+        <div className="flex justify-center w-full items-center gap-20">
+          <Card className="w-1/4 h-72 border-green-heading">
+            <CardHeader>
+              <CardTitle className="text-xl">Performance Prediction</CardTitle>
+              <CardDescription>
+                Based on previous reports and other relevant metrics
+              </CardDescription>
+              <Separator />
+              <Tilt>
+                <CardContent className="m-0 text-sm">
+                  <p>
+                    Performance Prediction leverages historical reports and
+                    pertinent metrics to anticipate future outcomes.
+                  </p>
+                  <p>
+                    By analyzing past performance trends and key indicators, our
+                    system generates insightful forecasts to aid in
+                    decision-making.
+                  </p>
+                </CardContent>
+              </Tilt>
+            </CardHeader>
+          </Card>
+          <Card className="w-1/4 h-72 border-green-heading">
+            <CardHeader>
+              <CardTitle className="text-xl">Interactive Learning</CardTitle>
+              <CardDescription>
+                Engaging content for both students and teachers
+              </CardDescription>
+              <Separator />
+              <Tilt>
+                <CardContent className="m-0 text-sm">
+                  <p>
+                    Our platform offers interactive learning experiences
+                    tailored for students and teachers alike.
+                  </p>
+                  <p>
+                    Designed with dyslexia-friendly features, such as clear
+                    fonts and color contrast, our platform ensures accessibility
+                    for all users.
+                  </p>
+                </CardContent>
+              </Tilt>
+            </CardHeader>
+          </Card>
+          <Card className="w-1/4 h-72 border-green-heading">
+            <CardHeader>
+              <CardTitle className="text-xl">Assessment and Feedback</CardTitle>
+              <CardDescription>
+                Streamlined evaluation processes for enhanced learning outcomes
+              </CardDescription>
+              <Separator />
+              <Tilt>
+                <CardContent className="m-0 text-sm">
+                  <p>
+                    Our platform simplifies assessment and feedback mechanisms,
+                    empowering teachers to provide timely and constructive
+                    feedback to students.
+                  </p>
+                  <p>
+                    {name} also provides, customizable assessment tools to
+                    automate the redundant processes.
+                  </p>
+                </CardContent>
+              </Tilt>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
+      <div className=" w-full h-screen flex gap-20 justify-between items-center">
+        <div className=" w-1/2 flex justify-center items-center mx-auto">
+          <Image
+            src={"/images/assessment.png"}
+            width={500}
+            height={500}
+            alt="assessment"
+          />
+        </div>
+        <div className="w-1/2  mx-auto">
+          <h1 className="font-semibold text-2xl">
+            Assessments,
+            <br />
+            Quizzes & Tests
+          </h1>
+          <p>
+            Easily launch live assignments, quizzes, and tests. <br />
+            Student results are automatically entered in the online gradebook.
+          </p>
+        </div>
+      </div>
+      <div className=" w-full h-screen grid grid-cols-2 justify-center items-center">
+        <div className="w-1/2  mx-auto">
+          <h1 className="font-semibold text-2xl">
+            One-on-One,
+            <br />
+            Discussions and tutoring
+          </h1>
+          <p>
+            Easily launch and schedule google meets, <br />
+            with faculty who are available.
+          </p>
+        </div>
+        <div className=" w-1/2 flex justify-center items-center mx-auto">
+          <Image
+            src={"/images/google-meet.png"}
+            width={500}
+            height={500}
+            alt="google meet"
+          />
+        </div>
+      </div>
+      <div className="bg-gradient-to-t from-green-dark to-white w-full h-screen flex gap-20 justify-between items-center">
+        <div className=" w-1/2 flex justify-center items-center mx-auto">
+          <Image
+            src={"/images/lady.png"}
+            width={500}
+            height={500}
+            alt="assessment"
+          />
+        </div>
+        <div className="w-1/2  mx-auto">
+          <h1 className="font-semibold text-2xl">
+            Tools for
+            <br />
+            Teachers & Students
+          </h1>
+          <p>
+            {name} has a dynamic set of teaching tools built to be deployed and <br />
+            used during class.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
