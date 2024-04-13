c80df36ec0a5b5554c81bfbcdad95f055620a98b
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2024-04-13 11:54:56 +0530

    Added ShadCN

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import { SignUp } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <main className="flex justify-center items-center mt-10">
+      <Button onClick={() => navigate("/login")}>Go to Login</Button>
       <SignUp />
     </main>
   );
